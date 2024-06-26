a58e4d3bc4f9987e065b953447580a341822fe6e
Sergio Arce <arce.roldan.sergio@gmail.com>
2024-06-26 00:57:41 -0600

    Previo dockerización

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
+import { Model } from 'mongoose';
 import { PokeoResponse } from './interfaces/poke-response.interface';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
-import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
+
+  
   constructor(
     @InjectModel( Pokemon.name )
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter
   ){}
 
-  private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
     await this.pokemonModel.deleteMany({});
-    const { data } = await this.axios.get<PokeoResponse>(`https://pokeapi.co/api/v2/pokemon?limit=650`);
+    const data = await this.http.get<PokeoResponse>(`https://pokeapi.co/api/v2/pokemon?limit=650`);
     const pokemonToInsert: {name: string, no: number }[] = [];
     data.results.forEach(( {name, url} ) => {
       const segments = url.split('/');
